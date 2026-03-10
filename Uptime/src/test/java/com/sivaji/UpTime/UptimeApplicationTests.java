2fb5dfe243a7fbcc106544b29ef814e62b8fda61
githubSivaji <123107544+githubSivaji@users.noreply.github.com>
2026-03-10 21:53:08 +0530

    first commit

---
+++ Uptime/src/test/java/com/sivaji/UpTime/UptimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivaji.UpTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
